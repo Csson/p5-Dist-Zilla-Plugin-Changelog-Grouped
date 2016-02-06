14d12309ad6c8799b67c9dd36bf53fa9c72fc6f1
Erik Carlsson <info@code301.com>
2016-02-06 14:56:45 +0000

    Init

---
+++ lib/Dist/Zilla/Plugin/Changelog/Grouped.pm
@@ -0,0 +1,28 @@
+use 5.14.0;
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::Changelog::Grouped;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::Changelog::Grouped;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::Plugin::Changelog::Grouped is ...
+
+=head1 SEE ALSO
+
+=cut
